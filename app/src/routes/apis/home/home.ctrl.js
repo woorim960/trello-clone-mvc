c99918390111fab2981bd756ee84ab097f879049
박우림 <woorimprog@gmail.com>
2021-06-17 05:39:52 +0900

    Add: List & Card 모델을 이용하여 응답 데이터 구축 #5

--- app/src/routes/apis/home/home.ctrl.js
+++ app/src/routes/apis/home/home.ctrl.js
@@ -1,8 +1,26 @@
 "use strict";
 
+const ListStorage = require("../../../models/ListStorage");
+const CardStorage = require("../../../models/CardStorage");
+
 const home = {
-  readAll: (req, res) => {
-    res.send("hello");
+  readAll: async (req, res) => {
+    const lists = await ListStorage.findAll();
+    const cards = await CardStorage.findAll();
+
+    const sortedCards = cards.reduce((newCards, card) => {
+      if (card.listNo in newCards)
+        newCards[card.listNo].push({ no: card.no, content: card.content });
+      else newCards[card.listNo] = [{ no: card.no, content: card.content }];
+      return newCards;
+    }, {});
+
+    const newLists = lists.map((list) => {
+      list.cards = sortedCards[list.no];
+      return list;
+    });
+
+    res.json({ lists: newLists });
   },
 };
 
