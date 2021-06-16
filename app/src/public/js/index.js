20ea43c59abb1dcd6047915abd6f3c83ee362de9
박우림 <woorimprog@gmail.com>
2021-06-17 01:50:08 +0900

    Close: List 등록 폼에서 Enter 누를 시 Save 버튼이 클릭되도록 구현 #3

--- app/src/public/js/index.js
+++ app/src/public/js/index.js
@@ -3,9 +3,10 @@
 const listBox = document.querySelector("div.list-box");
 
 listBox.addEventListener("click", changeListBox);
+listBox.addEventListener("keypress", keypressHandler);
 
 let isActiveListBox = false;
-function changeListBox() {
+function changeListBox(e) {
   if (!isActiveListBox) {
     listBox.classList.remove("list-box");
     listBox.classList.add("active-list-box");
@@ -18,4 +19,13 @@ function changeListBox() {
     `;
   }
   isActiveListBox = true;
+
+  if (e.target.classList[1] === "creation-btn")
+    console.log("리스트 제목을 등록합니다.");
+}
+
+function keypressHandler(e) {
+  if (e.key === "Enter") {
+    console.log("리스트 제목을 등록합니다.");
+  }
 }
