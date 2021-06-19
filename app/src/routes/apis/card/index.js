97ffc4577174968405fa12f47f81517b82a43a79
박우림 <woorimprog@gmail.com>
2021-06-19 23:06:38 +0900

    Close: card 삭제 API 구현 #5
    * src/models/CardStorage.js: card 삭제 메서드 구현
    * src/routes/apis/card/card.ctrl.js: card 삭제 컨트롤러 구현
    * src/routes/apis/card/index.js: 카드 삭제 라우트

--- app/src/routes/apis/card/index.js
+++ app/src/routes/apis/card/index.js
@@ -7,5 +7,6 @@ const ctrl = require("./card.ctrl");
 
 router.post("/", ctrl.card.create);
 router.put("/:no", ctrl.card.update);
+router.delete("/:no", ctrl.card.delete);
 
 module.exports = router;
