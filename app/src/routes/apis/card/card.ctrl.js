b5643d65c39360e217bce1cb24928ff74b2f4878
박우림 <woorimprog@gmail.com>
2021-06-19 23:00:10 +0900

    Add: card 수정 API 구현 #5
    * src/models/CardStorage.js: 카드 수정 메서드 구현
    * src/routes/apis/card/card.ctrl.js: 카드 수정 컨트롤러 구현
    * src/routes/apis/card/index.js: 카드 수정 라우트 구현
    * src/routes/apis/index.js: /api/cards/ 이하 모든 라우트 미들웨어 등록

--- app/src/routes/apis/card/card.ctrl.js
+++ app/src/routes/apis/card/card.ctrl.js
@@ -10,6 +10,18 @@ const card = {
 
     if (isSave) return res.status(201).json({ result: "success" });
   },
+
+  update: async (req, res) => {
+    const no = req.params.no;
+    const content = req.body.content;
+    const isUpdate = await CardStorage.update(no, content);
+
+    if (isUpdate) return res.status(204).end();
+    return res.status(404).json({
+      result: "error",
+      msg: "존재하지 않는 리소스입니다.",
+    });
+  },
 };
 
 module.exports = { card };
--- app/src/routes/apis/index.js
+++ app/src/routes/apis/index.js
@@ -6,5 +6,6 @@ const app = express();
 app.use("/home", require("./home"));
 app.use("/list", require("./list"));
 app.use("/card", require("./card"));
+app.use("/cards", require("./card"));
 
 module.exports = app;
