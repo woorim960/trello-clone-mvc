06e2348f9139aefc4c564c91ca905b080c0d22a4
박우림 <woorimprog@gmail.com>
2021-06-21 00:21:54 +0900

    Upgrade: 홈 API 응답시 생성 row의 id를 응답하도록 변경 #7

--- app/src/routes/apis/list/list.ctrl.js
+++ app/src/routes/apis/list/list.ctrl.js
@@ -5,9 +5,10 @@ const ListStorage = require("../../../models/ListStorage");
 const list = {
   create: async (req, res) => {
     const title = req.body.title;
-    const isSave = await ListStorage.save(title);
+    const insertId = await ListStorage.save(title);
 
-    if (isSave) return res.status(201).json({ result: "success" });
+    if (insertId)
+      return res.status(201).json({ result: "success", no: insertId });
     return res.status(404).json({
       result: "error",
       msg: "존재하지 않는 리소스입니다.",
