df8e45811c306b0f2145779f73f2738ae4681ac7
박우림 <woorimprog@gmail.com>
2021-06-21 02:04:21 +0900

    Refactor: List & Lists를 조작하는 컨트롤러 추가 #7
    * src/public/js/eventHandlers/ListHandler.js:
    (1) ListController를 이용한 코드로 변경
    (2) 새로운 List 노드의 id 값을 "-1" -> "new"로 변경
    
    * src/public/js/index.js:
    (1) List & Lists의 import 위치 변경
    (2) 새로운 List 노드의 id 값을 "-1" -> "new"로 변경
    
    * src/public/js/services/Lists/ListController.js:
    (1) saveNode() 추가
    (2) createActiveNode() 추가
    (3) changeToActiveNode() 추

---
+++ app/src/public/js/services/Lists/ListController.js
@@ -0,0 +1,27 @@
+"use strict";
+
+import List from "./List.js";
+import Http from "../../utils/Http.js";
+import lists from "../../index.js";
+
+export default class ListController {
+  static saveNode(title, id) {
+    const list = lists.getNodes("new");
+    list.changeToSavedNode(title, id);
+    lists.append(list, id);
+  }
+
+  static createActiveNode() {
+    const newList = new List("div");
+    const listForm = document.querySelector(".list-form");
+    listForm.appendChild(newList.node);
+    newList.changeToActiveNode();
+    lists.append(newList, "new");
+  }
+
+  static changeToActiveNode() {
+    const list = lists.getNodes("new");
+    if (!list.getIsActive()) list.changeToActiveNode();
+    list.setIsActive(true);
+  }
+}
