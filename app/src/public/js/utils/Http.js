effdfd8589aa0e9559993b27e915a261dd8d9ec3
박우림 <woorimprog@gmail.com>
2021-06-22 02:31:36 +0900

    Add: put 메서드 추가 #14

--- app/src/public/js/utils/Http.js
+++ app/src/public/js/utils/Http.js
@@ -20,4 +20,18 @@ export default class Http {
     if (stream.ok) var res = await stream.json();
     return { res, status: stream.status };
   }
+
+  static async put(url, body) {
+    const opt = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(body),
+    };
+
+    const stream = await fetch(url, opt);
+    if (stream.ok) var res = "PUT 요청은 응답 데이터가 없습니다.";
+    return { res, status: stream.status };
+  }
 }
